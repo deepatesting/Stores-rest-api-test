984457bd033e692943e52cd74642228cccd47eb4
Deepa P <79167426+deepatesting@users.noreply.github.com>
2021-04-08 14:18:11 +0100

    Update test_users.py

--- tests/system/test_users.py
+++ tests/system/test_users.py
@@ -1,7 +1,7 @@
 import json
 
-from pythonProject.starter_code.models.users import UserModel
-from pythonProject.starter_code.tests.base_test import BaseTest
+from models.users import UserModel
+from tests.base_test import BaseTest
 
 class UserTest(BaseTest):
     def test_register_user(self):
