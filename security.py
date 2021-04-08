7c67a395748ab7d1dd197247500e5285b26ef80d
Deepa P <79167426+deepatesting@users.noreply.github.com>
2021-04-08 14:22:20 +0100

    Update security.py

--- security.py
+++ security.py
@@ -1,5 +1,5 @@
 from werkzeug.security import safe_str_cmp
-from pythonProject.starter_code.models.users import UserModel
+from models.users import UserModel
 
 def authenticate(username, password):
     """
